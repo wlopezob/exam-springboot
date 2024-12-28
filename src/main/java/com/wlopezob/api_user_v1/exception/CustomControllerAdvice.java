e492d1913fbafec02ec18aa98120f938c2d4f749
DESKTOP-CSBAJF8\WLOPEZOB <wlopezob@gmail.com>
2024-12-28 11:39:50 -0500

    feat: add doc

--- src/main/java/com/wlopezob/api_user_v1/exception/CustomControllerAdvice.java
+++ src/main/java/com/wlopezob/api_user_v1/exception/CustomControllerAdvice.java
@@ -1,14 +1,11 @@
 package com.wlopezob.api_user_v1.exception;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
